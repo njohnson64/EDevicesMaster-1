610ffa716a2d1dd2bb99e2b840f075d9d72e9637
David Blocher <blocher.10@gmail.com>
2016-10-18 18:37:02 -0400

    Change instances of "Policy" to align with new name "PolicyEntity". Add migration and update database

--- CSCI-2630-Edevices-master/src/Entities/ClaimEntity.cs
+++ CSCI-2630-Edevices-master/src/Entities/ClaimEntity.cs
@@ -1,14 +1,15 @@
 ﻿
  using System;
  using System.ComponentModel.DataAnnotations.Schema;
- using EDeviceClaims.Entities;
+
+
 namespace EDeviceClaims.Entities
  {
    [Table("claims", Schema = "app")]
    public class ClaimEntity : EntityBase<Guid>
    {
     public Guid PolicyId { get; set; }
-     public virtual PolicyEntity Policy { get; set; }
+    public virtual PolicyEntity Policy { get; set; }
    }
  }
 
